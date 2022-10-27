be9eb0bedacb7064bc7277d3d7eb44524ab86314
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-10-27 15:54:31 +0300

    Completed the lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
-
-end
+# inherent user
+class Student < User
+    attr_reader :knowledge
+  
+    def initialize
+        # init empty array
+      @knowledge = []
+    end
+  
+    def learn(knowledge)
+      @knowledge << knowledge
+    end
+  
+  end
